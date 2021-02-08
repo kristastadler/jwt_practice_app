c68fd53a1caa059acbf4c5f95908095afefa1370
“Krista <stadler.krista@gmail.com>
2021-02-08 16:57:26 -0700

    added refreshToken

--- auth.js
+++ auth.js
@@ -20,6 +20,8 @@ const users = [
 
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const refreshTokenSecret = 'yourrefreshtokensecrethere';
+const refreshTokens = [];
 
 app.use(bodyParser.json());
 
@@ -35,12 +37,44 @@ app.post('/login', (req, res) => {
 
   if(user) {
     //Generate an access token
-    const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret);
+    const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+    const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+    refreshTokens.push(refreshToken);
 
     res.json({
-      accessToken
+      accessToken,
+      refreshToken
     });
   } else {
     res.send('Username or password incorrect');
   }
+});
+
+app.post('/token', (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  if (!refreshTokens.includes(token)) {
+    return res.sendStatus(403);
+  }
+  jwt.verify(token, refreshTokenSecret, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, {expiresIn: '20min'});
+
+    res.json({
+      accessToken
+    });
+  });
+});
+
+app.post('/logout', (req, res) => {
+  const { token } = req.body;
+  refreshTokens = refreshTokens.filter(token => t !== token);
+
+  res.send("Logout successful!");
 });
